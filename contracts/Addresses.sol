4cff8c8fb019435c945c9b53bd5d43ffa21c2264
exec009 <15655734+exec009@users.noreply.github.com>
2020-09-22 11:15:00 +0000

    Add address registry for stakeholder fund allocation

---
+++ contracts/Addresses.sol
@@ -0,0 +1,11 @@
+// contracts/Addresses.sol
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.2 <0.9.0;
+
+contract Addresses {
+    address public constant privateSale = 0xaaaa1111aaaa1111aaaa1111aaaa1111aaaa1111;
+    address public constant liquidityAndMM = 0xbbbb2222bbbb2222bbbb2222bbbb2222bbbb2222;
+    address public constant marketing = 0xcccc3333cccc3333cccc3333cccc3333cccc3333;
+    address public constant partnersAdvisors = 0xdddd4444dddd4444dddd4444dddd4444dddd4444;
+    address public constant reserves = 0xeeee5555eeee5555eeee5555eeee5555eeee5555;
+}
